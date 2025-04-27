c687de235bf1fa888d2f314f26186b9fd485f7d0
ethanny <145535621+ethanny@users.noreply.github.com>
2025-04-27 15:17:18 +0800

    added captcha

---
+++ myapp/src/Home/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style/home.css';
+
+function HomeScreen() {
+
+  return (
+    <div className='home-container'>
+      <h1>All books</h1>
+      <p>Welcome to the Home screen! This is where your main content goes.</p>
+    </div>
+  );
+}
+
+export default HomeScreen;
